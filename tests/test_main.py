b42cec0e62c071df41538d9051a2e821f82fce41
Antonio Valentino <antonio.valentino@tiscali.it>
2019-01-05 12:27:10 +0100

    Redirect output dring testing

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,7 +5,38 @@ import sys
 import unittest
 import subprocess
 
+if sys.version_info < (3, 0):
+    from io import BytesIO as StreamType
+else:
+    from io import StringIO as StreamType
+
+
 import pydups
+
+
+try:
+    from contextlib import redirect_stdout, redirect_stderr
+except ImportError:
+    class _redirect_stdstream(object):
+        _stream = None
+
+        def __init__(self, new_target):
+            self._new_target = new_target
+            self._old_targets = []
+
+        def __enter__(self):
+            self._old_targets.append(getattr(sys, self._stream))
+            setattr(sys, self._stream, self._new_target)
+            return self._new_target
+
+        def __exit__(self, exctype, excinst, exctb):
+            setattr(sys, self._stream, self._old_targets.pop())
+
+    class redirect_stdout(_redirect_stdstream):
+        _stream = "stdout"
+
+    class redirect_stderr(_redirect_stdstream):
+        _stream = "stderr"
 
 
 class MainTestCase(unittest.TestCase):
@@ -35,10 +66,14 @@ class MainTestCase(unittest.TestCase):
 
     def test_scan(self):
         argv = ['.']
-        ret = pydups.main(argv)
+        stream = StreamType()
+        with redirect_stdout(stream), redirect_stderr(stream):
+            ret = pydups.main(argv)
         assert ret == pydups.EX_OK
 
     def test_scan_invalid_input(self):
         argv = ['unexistent']
-        ret = pydups.main(argv)
+        stream = StreamType()
+        with redirect_stdout(stream), redirect_stderr(stream):
+            ret = pydups.main(argv)
         assert ret == pydups.EX_FAILURE
